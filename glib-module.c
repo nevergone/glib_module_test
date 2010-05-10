6dc4ed6e177c6fe446560dcd337f61f261704b01
Kurucz István <kurucz.istvan@gmail.com>
2010-05-10 11:18:24 +0200

    second version, corrected comments

--- glib-module.c
+++ glib-module.c
@@ -1,7 +1,25 @@
-/* glib-module.c  --  2006.04.01
-   Modulkeszites (pluginek hasznalata) GLib segitsegevel. */
+/*
+   glib-module.c  --  2006.04.01
+   plugin example with GLib: main application
+   compile: gcc -Wall glib-module.c `pkg-config --cflags --libs gtk+-2.0 gmodule-2.0` -o glib-module
+*/
 
-#include "glib-module.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <glib.h>
+#include <gmodule.h>
+#include <gtk/gtk.h>
+
+GtkWidget * main_window = NULL;
+GModule * module = NULL;
+gchar * module_path;
+gint module_results = 0;
+int (*module_func) (gchar *);
+
+gint delete_event (GtkWidget * widget,  GdkEvent * event,  gpointer data)  {
+  gtk_main_quit ();
+  return FALSE;
+}
 
 void main_window_create ()  {
   main_window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
@@ -9,42 +27,35 @@ void main_window_create ()  {
   gtk_window_set_title (GTK_WINDOW(main_window), "glib-module test");
 }
 
-
-gint delete_event (GtkWidget * widget,  GdkEvent * event,  gpointer data)  {
-  gtk_main_quit ();
-  return FALSE;
-}
-
-
 int main (int argc, char * argv[])  {
   gtk_init (&argc, &argv);
-  /* ha a modulkezeles nem tamogatott a rendszeren */
+  /* if plugins not supported */
   if (!g_module_supported())  {
     perror ("module not supported");
     return EXIT_FAILURE;
   }
-  modul_path = g_new0 (gchar, 200);
-  modul_path = g_module_build_path ("plugins", "plug_01");
-  modul = g_module_open (modul_path, G_MODULE_BIND_LAZY);
-  if (modul != NULL)  g_message ("open\n");
-	else {
-	  g_error ("modulpath nem talalhato: %s", modul_path);
-    return EXIT_FAILURE;
-	}
-  g_message ("modul neve: %s\n", g_module_name (modul));
-  g_message ("modul path: %s\n", modul_path);
-  if (g_module_symbol (modul, "modul_teszt", (gpointer*)&modul_fuggveny))  g_message ("symbol\n");
-	else {
-	  g_error ("szimbolum nem talalhato");
-    return EXIT_FAILURE;
-	}
-
-  modul_visszateresi_erteke = modul_fuggveny ("fuggveny_parameter");
-  g_message ("modul visszateresi erteke: %d\n", modul_visszateresi_erteke);
-  if (g_module_close (modul))  g_message ("close\n");
-  g_free (modul_path);
+  module_path = g_new0 (gchar, 200);
+  module_path = g_module_build_path ("plugins", "plug_01");
+  module = g_module_open (module_path, G_MODULE_BIND_LAZY);
+  if (module != NULL)  g_message ("open\n");
+  else {
+        g_error ("module path not found: %s", module_path);
+        return EXIT_FAILURE;
+  }
+  g_message ("module name: %s\n", g_module_name (module));
+  g_message ("module path: %s\n", module_path);
+  if (g_module_symbol (module, "module_test", (gpointer*)&module_func))  g_message ("symbol\n");
+  else {
+        g_error ("symbol not found");
+        return EXIT_FAILURE;
+  }
+  module_results = module_func ("function_parameter");
+  g_message ("module results: %d\n", module_results);
+  if (g_module_close (module))  g_message ("close\n");
+  g_free (module_path);
   main_window_create ();
   gtk_widget_show_all (main_window);
   gtk_main ();
   return EXIT_SUCCESS;
 }
+
